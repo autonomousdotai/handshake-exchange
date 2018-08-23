43b672233d35f3681f732468c3468c4f83f72325
Khoa Tran <khoa@autonomous.nyc>
2018-08-23 14:00:25 +0700

    Update CC info

--- integration/bitcoin_service/integration.go
+++ integration/bitcoin_service/integration.go
@@ -1,10 +1,94 @@
 package bitcoin_service
 
-import "github.com/shopspring/decimal"
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
+	"github.com/shopspring/decimal"
+	"math/big"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
 type BitcoinService struct {
 }
 
-func (c *BitcoinService) SendTransaction(address string, amount decimal.Decimal) (string, error) {
-	return "", nil
+type Transaction struct {
+	TxId               string `json:"txid"`
+	SourceAddress      string `json:"source_address"`
+	DestinationAddress string `json:"destination_address"`
+	Amount             int64  `json:"amount"`
+	UnsignedTx         string `json:"unsignedtx"`
+	SignedTx           string `json:"signedtx"`
+}
+
+var BTC_IN_SATOSHI = decimal.NewFromBigInt(big.NewInt(100000000), 0)
+
+func (c *BitcoinService) SendTransaction(address string, amount decimal.Decimal) (Transaction, error) {
+	var transaction Transaction
+	wif, err := btcutil.DecodeWIF(os.Getenv("BTC_KEY"))
+	if err != nil {
+		return Transaction{}, err
+	}
+	i := 0
+	txHash := ""
+	for i < 4 {
+		txHash += fmt.Sprintf("%s", strconv.FormatUint(rand.New(rand.NewSource(time.Now().UnixNano())).Uint64(), 16))
+		i += 1
+	}
+	fmt.Println(txHash)
+	sendAmount := amount.Mul(BTC_IN_SATOSHI).IntPart()
+	addresspubkey, _ := btcutil.NewAddressPubKey(wif.PrivKey.PubKey().SerializeUncompressed(), &chaincfg.MainNetParams)
+	sourceTx := wire.NewMsgTx(wire.TxVersion)
+	sourceUtxoHash, _ := chainhash.NewHashFromStr(txHash)
+	sourceUtxo := wire.NewOutPoint(sourceUtxoHash, 0)
+	sourceTxIn := wire.NewTxIn(sourceUtxo, nil, nil)
+	destinationAddress, err := btcutil.DecodeAddress(address, &chaincfg.MainNetParams)
+	sourceAddress, err := btcutil.DecodeAddress(addresspubkey.EncodeAddress(), &chaincfg.MainNetParams)
+	if err != nil {
+		return Transaction{}, err
+	}
+	destinationPkScript, _ := txscript.PayToAddrScript(destinationAddress)
+	sourcePkScript, _ := txscript.PayToAddrScript(sourceAddress)
+	sourceTxOut := wire.NewTxOut(sendAmount, sourcePkScript)
+	sourceTx.AddTxIn(sourceTxIn)
+	sourceTx.AddTxOut(sourceTxOut)
+	sourceTxHash := sourceTx.TxHash()
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+	prevOut := wire.NewOutPoint(&sourceTxHash, 0)
+	redeemTxIn := wire.NewTxIn(prevOut, nil, nil)
+	redeemTx.AddTxIn(redeemTxIn)
+	redeemTxOut := wire.NewTxOut(sendAmount, destinationPkScript)
+	redeemTx.AddTxOut(redeemTxOut)
+	sigScript, err := txscript.SignatureScript(redeemTx, 0, sourceTx.TxOut[0].PkScript, txscript.SigHashAll, wif.PrivKey, false)
+	if err != nil {
+		return Transaction{}, err
+	}
+	redeemTx.TxIn[0].SignatureScript = sigScript
+	flags := txscript.StandardVerifyFlags
+	vm, err := txscript.NewEngine(sourceTx.TxOut[0].PkScript, redeemTx, 0, flags, nil, nil, sendAmount)
+	if err != nil {
+		return Transaction{}, err
+	}
+	if err := vm.Execute(); err != nil {
+		return Transaction{}, err
+	}
+	var unsignedTx bytes.Buffer
+	var signedTx bytes.Buffer
+	sourceTx.Serialize(&unsignedTx)
+	redeemTx.Serialize(&signedTx)
+	transaction.TxId = sourceTxHash.String()
+	transaction.UnsignedTx = hex.EncodeToString(unsignedTx.Bytes())
+	transaction.Amount = sendAmount
+	transaction.SignedTx = hex.EncodeToString(signedTx.Bytes())
+	transaction.SourceAddress = sourceAddress.EncodeAddress()
+	transaction.DestinationAddress = destinationAddress.EncodeAddress()
+	return transaction, nil
 }
--- service/creditcard.go
+++ service/creditcard.go
@@ -215,7 +215,7 @@ func (s CreditCardService) PayInstantOffer(userId string, offerBody bean.Instant
 		bean.LTC.Code: 3,
 		bean.BCH.Code: 4,
 	}
-	description := fmt.Sprintf("User %s buys %s of %d", userId, offerBody.Amount, mapCrypto[offerBody.Currency])
+	description := fmt.Sprintf("%s of %d", offerBody.Amount, mapCrypto[offerBody.Currency])
 
 	stripeCharge, err := stripe_service.Charge(token, paymentMethodData.Token, fiatAmount, statement, description)
 	if ce.SetError(api_error.ChargeCCFailed, err) {
@@ -584,7 +584,7 @@ func (s CreditCardService) ScriptCheckFailedTransfer() error {
 					payment := paymentTO.Object.(bean.CCTransaction)
 
 					//fmt.Println(fmt.Sprintf("%s,%s,%s,%s", instantOffer.Amount, instantOffer.Currency, instantOffer.Address, data[0:80]))
-					fmt.Println(fmt.Sprintf("%s,%s,%s", instantOffer.FiatAmount, payment.ExternalId, data[0:80]))
+					fmt.Println(fmt.Sprintf("%s,%s,%s,%s,%s", instantOffer.FiatAmount, payment.ExternalId, instantOffer.Currency, instantOffer.Address, data[0:80]))
 				}
 			}
 		}
