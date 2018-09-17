7365bcfef97c946003e8c296e9c9d3ee4bf1a862
Khoa Tran <khoa@autonomous.nyc>
2018-09-17 11:13:29 +0700

    Add CC user limit back

--- service/creditcard.go
+++ service/creditcard.go
@@ -211,11 +211,11 @@ func (s CreditCardService) PayInstantOffer(userId string, offerBody bean.Instant
 	}
 
 	// Check CC limit - remove user limit
-	//ccLimitCE := UserServiceInst.CheckCCLimit(offerBody.UID, offerBody.FiatAmount)
-	//if ccLimitCE.HasError() {
-	//	ce.SetError(api_error.CCOverLimit, ccLimitCE.Error)
-	//	return
-	//}
+	ccLimitCE := UserServiceInst.CheckCCLimit(offerBody.UID, offerBody.FiatAmount)
+	if ccLimitCE.HasError() {
+		ce.SetError(api_error.CCOverLimit, ccLimitCE.Error)
+		return
+	}
 
 	if paymentMethodData.Token == "true" {
 		//token = ""
